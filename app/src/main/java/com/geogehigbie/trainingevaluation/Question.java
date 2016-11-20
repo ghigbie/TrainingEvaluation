ae9ee80dcfb7ed8b7656e1cd7f3942e5b4be48f4
George Higbie <gehigbie@gmail.com>
2016-11-20 16:08:34 -0500

    initial commit

---
+++ app/src/main/java/com/geogehigbie/trainingevaluation/Question.java
@@ -0,0 +1,17 @@
+package com.geogehigbie.trainingevaluation;
+
+/**
+ * Created by georgehigbie on 11/20/16.
+ */
+
+public class Question {
+
+    private String QuestionString;
+    private int Rating;
+
+    public Question(String questionString, int rating){
+
+        questionString = QuestionString;
+        rating = Rating;
+    }
+}
