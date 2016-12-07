164dfd5fed27e931cfcf503f1682c7e8324167c3
George Higbie <gehigbie@gmail.com>
2016-12-07 18:16:24 -0500

    added some important comments

--- app/src/main/java/com/geogehigbie/trainingevaluation/AdminActivity.java
+++ app/src/main/java/com/geogehigbie/trainingevaluation/AdminActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class AdminActivity extends AppCompatActivity {
 
+    //this activity will get information from firebase and then summarize it
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
